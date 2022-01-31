9e2e3afd573ffd43651cbaabe0a1d23a65d5858c
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-01-31 19:00:54 +0100

    using Newtonsoft.Json

--- AzureCosmosDB/Models/Item.cs
+++ AzureCosmosDB/Models/Item.cs
@@ -1,19 +1,19 @@
-﻿using System.Text.Json.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace code_maze_cosmosdb.Models
 {
     public class Item
     {
-        [JsonPropertyName("id")]
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
-        [JsonPropertyName("name")]
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("description")]
+        [JsonProperty(PropertyName = "description")]
         public string Description { get; set; }
 
-        [JsonPropertyName("isComplete")]
+        [JsonProperty(PropertyName = "isComplete")]
         public bool Completed { get; set; }
     }
 }
